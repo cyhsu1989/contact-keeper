b16cc8a4c6100df3dc61c929211700413276c01c
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-29 18:57:25 +0800

    [Create & Respond With JSON Web Token] 建立 JSON Web Token - 使用 jsonwebtoken 套件。 - 建立 payload 到 JWT，這個 payload 是個物件，存著 user ID。 - 簽署 JSON Web Token，有 4 個參數：payload、secret、object of options、callback。其中 secret 存在 default.json，用 config 取得。 - 成功，就回傳簽好的 token。 - 後續就能透過這個 user ID 去驗證並去做任何操作。

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../models/User");
 
@@ -48,7 +50,29 @@ router.post(
 			user.password = await bcrypt.hash(password, salt);
 
 			await user.save();
-			res.send("User saved");
+
+			// 建立 payload 到 JWT
+			// 這個 payload 是個物件，存著 user ID
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			// 簽署 JSON Web Token，有 4 個參數：payload、secret、object of options、callback
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{
+					expiresIn: 360000
+				},
+				(err, token) => {
+					// 有錯誤，就丟出錯誤
+					if (err) throw err;
+					// 成功，就回傳簽好的 token
+					res.json({ token });
+				}
+			);
 		} catch (error) {
 			// 詳細的 error 資訊只給開發人員知道
 			console.log(error.message);
