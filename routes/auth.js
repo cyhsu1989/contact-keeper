ca709a051280b8b98b65c0a82c62f938afc93eea
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-30 15:25:22 +0800

    [Authenticate Route] - 驗證兩個資料的格式正確性。 - 將 request 的資料拿出來，與 DB 中的資料比對。 - 建立並回傳 Token 流程。

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = require("../models/User");
 
 //@route    GET api/auth
 //@desc     Get logged in user
@@ -11,8 +17,58 @@ router.get("/", (req, res) => {
 //@route    POST api/auth
 //@desc     Auth user & get token
 //@access   Public
-router.post("/", (req, res) => {
-	res.send("Log in user");
-});
+router.post(
+	"/",
+	[
+		check("email", "Please include a valid email").isEmail(),
+		check("password", "Password is required").exists()
+	],
+	async (req, res) => {
+		// 驗證兩個資料的格式正確性
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			// 這個 request 的 email，檢查是否有在 DB 中找到
+			let user = await User.findOne({ email });
+			if (!user) {
+				return res.status(400).json({ msg: "Invalid Crendentials" });
+			}
+
+			// request 中的 password 與 DB 中的 password 比較
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res.status(400).json({ msg: "Invalid Crendentials" });
+			}
+
+			// 建立並回傳 Token 流程
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{
+					expiresIn: 360000
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
 
 module.exports = router;
