dc79588ce0f035615cd9b9c429fe2e19860c7c13
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-31 15:02:15 +0800

    [Contacts Context & Global State] - 建立 `/context` 資料夾管理 context 相關檔案。 - 針對每個檔案初始化。

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,28 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./component/layout/Navbar";
 import About from "./component/pages/About";
 import Home from "./component/pages/Home";
+import ContactState from "./context/contact/ContactState";
 import "./App.css";
 
 function App() {
 	return (
-		<Router>
-			<Fragment>
-				<Navbar></Navbar>
-				<div className="container">
-					<Switch>
-						<Route exact path="/" component={Home}></Route>
-						<Route exact path="/about" component={About}></Route>
-					</Switch>
-				</div>
-			</Fragment>
-		</Router>
+		<ContactState>
+			<Router>
+				<Fragment>
+					<Navbar></Navbar>
+					<div className="container">
+						<Switch>
+							<Route exact path="/" component={Home}></Route>
+							<Route
+								exact
+								path="/about"
+								component={About}
+							></Route>
+						</Switch>
+					</div>
+				</Fragment>
+			</Router>
+		</ContactState>
 	);
 }
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,70 @@
+import React, { useReducer } from "react";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+
+import uuid from "uuid";
+import {
+	ADD_CONTACT,
+	DELETE_CONTACT,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	UPDATE_CONTACT,
+	FILTER_CONTACTS,
+	CLEAR_FILTER
+} from "../type";
+
+const ContactState = props => {
+	const initalState = {
+		contacts: [
+			{
+				type: "personal",
+				id: 1,
+				name: "Harry White",
+				email: "harry@gmail.com",
+				phone: "333-333-333"
+			},
+			{
+				type: "personal",
+				id: 2,
+				name: "Ted Johnson",
+				email: "ted@gmail.com",
+				phone: "222-222-2222"
+			},
+			{
+				type: "professional",
+				id: 3,
+				name: "Sara Smith",
+				email: "ssmith@gmail.com",
+				phone: "111-111-1111"
+			}
+		]
+	};
+
+	const [state, dispatch] = useReducer(contactReducer, initalState);
+
+	// Add Contact
+
+	// Delete Contact
+
+	// Set Current Contact
+
+	// Clear Current Contact
+
+	// Update Contact
+
+	// Filter Contact
+
+	// Clear Filter
+
+	return (
+		<ContactContext.Provider
+			value={{
+				contacts: state.contacts
+			}}
+		>
+			{props.children}
+		</ContactContext.Provider>
+	);
+};
+
+export default ContactState;
