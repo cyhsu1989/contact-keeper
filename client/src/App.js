63279af38e8164bd542bfbc3eea7b2b486eab5c1
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-10-31 14:23:13 +0800

    [Cleanup, Navbar & Router Setup] - 清除預設檔案。 - 安裝前端套件。 - 新增 Navbar、Home、About Component。 - 設定 Route：`/`、`/about`。

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./component/layout/Navbar";
+import About from "./component/pages/About";
+import Home from "./component/pages/Home";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Fragment>
+				<Navbar></Navbar>
+				<div className="container">
+					<Switch>
+						<Route exact path="/" component={Home}></Route>
+						<Route exact path="/about" component={About}></Route>
+					</Switch>
+				</div>
+			</Fragment>
+		</Router>
+	);
 }
 
 export default App;
