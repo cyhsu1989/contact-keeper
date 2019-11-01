67b28a45ba2aefe9492d2ed241fe169bf6409abc
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-11-01 15:00:44 +0800

    [Auth Context & Initial State] - 新增 `/context/auth` 資料夾，管理登錄驗證相關檔案。 - 新增 type。 - 建立三個檔案 * `context/auth/authContext.js` * `context/auth/authReducer.js` * `context/auth/AuthState.js`

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,30 @@ import Navbar from "./component/layout/Navbar";
 import About from "./component/pages/About";
 import Home from "./component/pages/Home";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 import "./App.css";
 
 function App() {
 	return (
-		<ContactState>
-			<Router>
-				<Fragment>
-					<Navbar></Navbar>
-					<div className="container">
-						<Switch>
-							<Route exact path="/" component={Home}></Route>
-							<Route
-								exact
-								path="/about"
-								component={About}
-							></Route>
-						</Switch>
-					</div>
-				</Fragment>
-			</Router>
-		</ContactState>
+		<AuthState>
+			<ContactState>
+				<Router>
+					<Fragment>
+						<Navbar></Navbar>
+						<div className="container">
+							<Switch>
+								<Route exact path="/" component={Home}></Route>
+								<Route
+									exact
+									path="/about"
+									component={About}
+								></Route>
+							</Switch>
+						</div>
+					</Fragment>
+				</Router>
+			</ContactState>
+		</AuthState>
 	);
 }
 
