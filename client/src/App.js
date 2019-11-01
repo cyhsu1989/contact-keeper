08560074e2bcabd83488d1b7e4d579ef9bda978f
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-11-01 15:48:05 +0800

    [Register & Login Forms] - 新增 `Register`、`Login` component，將登入表單畫面先做出來。 - 增加 `Register`、`Login` 的路由。

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./component/layout/Navbar";
 import About from "./component/pages/About";
 import Home from "./component/pages/Home";
+import Register from "./component/auth/Register";
+import Login from "./component/auth/Login";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
 import "./App.css";
@@ -21,6 +23,16 @@ function App() {
 									exact
 									path="/about"
 									component={About}
+								></Route>
+								<Route
+									exact
+									path="/register"
+									component={Register}
+								></Route>
+								<Route
+									exact
+									path="/login"
+									component={Login}
 								></Route>
 							</Switch>
 						</div>
